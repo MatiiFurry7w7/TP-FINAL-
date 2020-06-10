1e83e0bbc0d8483293c480d81d2d0e0c81f50cf5
MatiiFurry7w7 <66691079+MatiiFurry7w7@users.noreply.github.com>
2020-06-10 08:40:54 -0300

    Add files via upload

--- TP FINAL/main.c
+++ TP FINAL/main.c
@@ -4,29 +4,30 @@
 #include <string.h>
 
 #define ESC 27
+#define fileCL "clientes.bin"
 
 typedef struct
 {
-int id; /// AUTOINCREMENTAL UNICO
-char nombre[30];
-char apellido[30];
-int dni;
-char email[30];
-char domicilio[45];
-int movil;
-int baja;
-}stCliente;
+    int id; /// AUTOINCREMENTAL UNICO
+    char nombre[30];
+    char apellido[30];
+    int dni;
+    char email[30];
+    char domicilio[45];
+    int movil;
+    int baja;
+} stCliente;
 
 typedef struct
 {
-int id;              /// AUTOINCREMENTAL UNICO
-int idCliente;
-int anio;
-int mes;             /// 1 - 12
-int dia;             /// 1 - … (DEPENDE DEL MES)
-int datosConsumidos; /// MBs.
-int baja;            /// 0 = ACTIVO / 1 = INACTIVO
-}stConsumos;
+    int id;              /// AUTOINCREMENTAL UNICO
+    int idCliente;
+    int anio;
+    int mes;             /// 1 - 12
+    int dia;             /// 1 - … (DEPENDE DEL MES)
+    int datosConsumidos; /// MBs.
+    int baja;            /// 0 = ACTIVO / 1 = INACTIVO
+} stConsumos;
 
 ///Prototipados
 
@@ -34,8 +35,17 @@ void menu();
 void subcli();
 void subcon();
 
+int validaEmail (char email[]);
+int cantRegistros();
+int validaExistencia(int dni);
+void guardarCliente(stCliente c);
+
+stCliente cargarCliente();
+
 int main()
 {
+
+stCliente cliente;
 
 ///Menu
     char opcion;
@@ -65,6 +75,8 @@ int main()
                 {
                 case 49: /// opcion 1
                     printf("\t\t\t\t<<<<---ALTA DE CLIENTES--->>>");
+
+                    guardarCliente(cargarCliente());
 
                     printf("\n\n\n");
                     system("pause");
@@ -159,7 +171,7 @@ int main()
     system("cls");
     system("color a");
     printf("\n\n\n\n\n\n\n\t\t\t<<<<---GRACIAS POR ELEGIR UT MOVIL, ESPERAMOS VERLO/A PRONTO!--->>>\n\n\n\n\n\n\n\n\n\n\n");
-      puts("\n\n\n\n\t------------------------------------------------------------------------------------------------------\n");
+    puts("\n\n\n\n\t------------------------------------------------------------------------------------------------------\n");
     printf("\tCOMICOS CUBICOS. TM - 2020");
     getch();
     system("cls");
@@ -167,6 +179,7 @@ int main()
     return 0;
 }
 
+/// MENU ///
 
 void menu()
 {
@@ -175,7 +188,7 @@ void menu()
     printf("\t-----------------\n\n");
     printf("\t[1] - SUBMENU CLIENTES.\n\n");
     printf("\t[2] - SUBMENU CONSUMOS.\n\n");
-      puts("\n\n\n\t------------------------------------------------------------------------------\n");
+    puts("\n\n\n\t------------------------------------------------------------------------------\n");
     printf("\tCOMICOS CUBICOS. TM - 2020");
     printf("\t\t\t\t\t[ESC] TO QUIT.");
 }
@@ -190,7 +203,7 @@ void subcli()
     printf("\t[3] - MODIFICACION DE CLIENTES.\n");
     printf("\t[4] - LISTAR CLIENTES ACTIVOS.\n");
     printf("\t[5] - LISTAR CLIENTES INACTIVOS.\n");
-      puts("\n\n\t------------------------------------------------------------------------------\n");
+    puts("\n\n\t------------------------------------------------------------------------------\n");
     printf("\tCOMICOS CUBICOS. TM - 2020");
     printf("\t\t\t      [6] RETURN TO MAIN MENU.");
 }
@@ -205,10 +218,154 @@ void subcon()
     printf("\t[3] - MODIFICACION DE CONSUMO.\n");
     printf("\t[4] - LISTADO DE CONSUMOS.\n");
     printf("\t[5] - ESTADISTICAS DE LOS CONSUMOS.\n");
-      puts("\n\n\t------------------------------------------------------------------------------\n");
+    puts("\n\n\t------------------------------------------------------------------------------\n");
     printf("\tCOMICOS CUBICOS. TM - 2020");
     printf("\t\t\t      [6] RETURN TO MAIN MENU.");
 }
+
+/// FUNCIONES ///
+
+stCliente cargarCliente()
+{
+    stCliente c;
+
+    int valem;
+
+    c.id = cantRegistros();
+
+    printf("\nNombre: ");
+    fflush(stdin);
+    gets(c.nombre);
+
+    printf("\nApellido: ");
+    fflush(stdin);
+    gets(c.apellido);
+
+    printf("\nDNI: ");
+    scanf("%d", &c.dni);
+
+    c.dni = validaExistencia(c.dni);
+
+    printf("\nEmail: ");
+    fflush(stdin);
+    gets(c.email);
+    valem = validaEmail(c.email);
+
+    while(!valem)
+    {
+        system("cls");
+        printf("\nERROR: Email invalido, ingrese nuevamente: ");
+        fflush(stdin);
+        gets(c.email);
+        valem = validaEmail(c.email);
+
+    }
+
+    printf("\nDomicilio: ");
+    fflush(stdin);
+    gets(c.domicilio);
+
+    printf("\nT. Celular: ");
+    scanf("%d", &c.movil);
+
+    c.baja = 0;
+
+    return c;
+}
+
+void guardarCliente(stCliente c)
+{
+  FILE *pArchCliente = fopen(fileCL, "ab");
+  if(pArchCliente)
+  {
+   fwrite(&c, sizeof(stCliente), 1, pArchCliente);
+  }
+  fclose(pArchCliente);
+}
+
+int cantRegistros()
+{
+ stCliente c;
+ int cont=0;
+
+ FILE *pArchCliente = fopen(fileCL, "rb");
+ if(pArchCliente)
+ {
+   fseek(pArchCliente, 0, SEEK_END);
+   cont = ftell(pArchCliente)/sizeof(stCliente);
+  fclose(pArchCliente);
+ }
+
+return cont;
+}
+
+/// VALIDACIONES ///
+
+int validaEmail(char email[])
+{
+
+    int validos = strlen(email);
+    int i=0;
+    int flag = 0;
+
+    while (i<validos && flag == 0)
+    {
+        if (email[i] == 64)
+        {
+            flag = 1;
+        }
+        i++;
+    }
+    return flag;
+}
+
+int validaExistencia(int dni)
+{
+ stCliente c;
+
+ FILE *pArchCliente = fopen(fileCL, "rb");
+
+ if(pArchCliente)
+ {
+ while(dni == c.dni)
+ {
+  while(fread(&c, sizeof(stCliente), 1, pArchCliente) > 0)
+   {
+    system("cls");
+    printf("\nEse DNI ya existe en la base de datos, ingrese otro: ");
+    scanf("%d", &dni);
+   }
+  }
+  fclose(pArchCliente);
+ }
+
+return dni;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
